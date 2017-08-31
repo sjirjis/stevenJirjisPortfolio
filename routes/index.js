0208e0f0bbc6c237008a30b4f9d468ad1b88e52b
SJIRJIS <sjirjis@gmail.com>
2017-08-30 18:56:53 -0700

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+﻿
+/*
+ * GET home page.
+ */
+
+exports.index = function (req, res) {
+    res.render('index', { title: 'my home', year: new Date().getFullYear() });
+};
+
+exports.contact = function (req, res) {
+    res.render('contact', { title: 'my contact', year: new Date().getFullYear(), message: 'Your contact page' });
+};
